488144456aee3f3bc30f057aa0d551f8cd0ad2eb
ariesoeteman <awsoeteman@hotmail.com>
2018-01-10 11:39:20 +0100

    nie zo veel

--- regressie.py
+++ regressie.py
@@ -15,18 +15,25 @@ from keras.optimizers import Adam
 from keras.models import Model
 from keras import backend as K
 
-# Load the data
-input_train = pd.read_table("../train.tsv")
-input_train_txt = input_train.as_matrix()
-target_train = input_train['price']
-input_test = pd.read_table("../test.tsv")
 
-print("Data is loaded")
+# Packages van "Tensorflow starter conv1d"
+# import re
+# from collections import Counter
+# import tensorflow as tf
+# from nltk.stem.porter import PorterStemmer
+# from fastcache import clru_cache as lru_cache
+# from time import time
 
 def error_function(labels, predicted):
     # Y and Y_red have already been in log scale.
     assert labels.shape == predicted.shape
     return np.sqrt(np.mean(np.square(predicted - labels)))
+
+# Load the data
+input_train = pd.read_table("../train.tsv")
+input_train_txt = input_train.as_matrix()
+target_train = input_train['price']
+print("Data is loaded")
 
 # Returns encoded discrete values of a single column
 def label_encoder(column):
@@ -36,9 +43,10 @@ def label_encoder(column):
 	del le
 	return encoded_column
 
-# Basic regression (taken from http://scikit-learn.org/stable/modules/linear_model.html)
-# Only uses the numeric variables (i.e. condition and shipping method)
-reg = linear_model.LinearRegression()
-reg.fit(np.array([input_train_txt[:,2],input_train_txt[:,6]]).T, target_train)
-first_prediction = reg.predict()
+# Testing the label encoder
+print (input_train['category_name'].iloc[0:3])
+print label_encoder(input_train['category_name'].iloc[0:3])
 
+# Basic regression (taken from http://scikit-learn.org/stable/modules/linear_model.html)
+reg = linear_model.LinearRegression()
+reg.fit(input_train, target_train)
