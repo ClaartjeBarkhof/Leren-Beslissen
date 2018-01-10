ce10a478971a96978179b6cca8ca42bb66e6a1bd
kassa <kassanderink@gmail.com>
2018-01-10 11:18:11 +0100

    basic things

--- regressie.py
+++ regressie.py
@@ -2,8 +2,8 @@
 import numpy as np
 import pandas as pd
 
+from sklearn import linear_model
 from sklearn.preprocessing import LabelEncoder
-from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import Ridge
 from sklearn.pipeline import FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -15,12 +15,19 @@ from keras.optimizers import Adam
 from keras.models import Model
 from keras import backend as K
 
-input_train = pd.read_table("../train.tsv")
-input_train_txt = input_train.as_matrix()
-target_train = input_train['price']
-print(target_train.shape)
-
 def error_function(labels, predicted):
     # Y and Y_red have already been in log scale.
     assert Y.shape == Y_pred.shape
     return np.sqrt(np.mean(np.square(Y_pred - Y )))
+
+# Load the data
+input_train = pd.read_table("../train.tsv")
+input_train_txt = input_train.as_matrix()
+target_train = input_train['price']
+print("Data is loaded")
+print(input_train[0:1])
+
+# Basic regression (taken from http://scikit-learn.org/stable/modules/linear_model.html)
+reg = linear_model.LinearRegression()
+reg.fit(input_train, target_train)
+
