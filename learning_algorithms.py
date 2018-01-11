3b227ef91ef30178b8f2153cf481731ab3f77901
kassa <kassanderink@gmail.com>
2018-01-11 13:49:07 +0100

    Nieuw bestand met alle algoritmes

---
+++ learning_algorithms.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+from sklearn import neural_network
+from sklearn import linear_model
+
+def linear_regression(training_set, training_target, validation_set, validation_target):
+    reg = linear_model.LinearRegression()
+    reg.fit(training_set, training_target)
+    prediction = reg.predict(validation_set)
+    return pd.Dataframe({'p':prediction, 'a':validation_target})
+
+def ann_regression(training_set, training_target, validation_set, validation_target):
+	prediction_model = neural_network.MLPRegressor()
+	prediction_model.fit(train_data, train_target)
+	prediction = prediction_model.predict(val_data)
+    return pd.Dataframe({'p':prediction, 'a':validation_target})
