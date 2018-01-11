15c78febf31b47c32266441aea0e63c337de6daf
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2018-01-11 11:23:52 +0100

    tokenize start

--- clean.py
+++ clean.py
@@ -1,8 +1,16 @@
 import pandas as pd
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
+tokenizer = RegexpTokenizer(r'\w+')
+stop_words = set(stopwords.words('english'))
 
 def open_tsv(filepath):
 	data = pd.read_table(filepath)
-	return data#.iloc[0:10,:]
+	return data.iloc[0:10,:]
 
 def replace_NAN(data):
 	data['category_name'] = data['category_name'].fillna('undefined').astype(str)
@@ -14,6 +22,23 @@ def split_catagories(data):
 	column_split = lambda x: pd.Series([i for i in (x.split('/'))])
 	splitted = data['category_name'].apply(column_split)
 	data = pd.concat([data, splitted], axis=1)
+	return data
+
+def tokenize(description):
+    try:
+        split = tokenizer.tokenize(description)
+        filtered_sentence = []
+        #filtered_sentence1 = [w.lower() for w in split if not w in stop_words]
+        filtered_sentence = [ps.stem(w.lower()) for w in split if not w in stop_words]
+        # filtered_sentence = set(filtered_sentence) In case you would want to remove the duplicates.
+        return filtered_sentence
+    except: 
+        print("error with description:", description)
+        return []
+
+def add_tokenize_cols(data):
+	data['tokenized_description'] = data['item_description'].apply(lambda x: tokenize(x))
+	data['description_len'] = data['tokenized_description'].apply(lambda x: len(x))
 	return data
 
 def main():
