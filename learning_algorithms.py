29ff5e1fd3378b53652ef56da22b4ad3a4edddfb
kassa <kassanderink@gmail.com>
2018-01-11 13:55:21 +0100

    asdf

--- learning_algorithms.py
+++ learning_algorithms.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 from sklearn import neural_network
 from sklearn import linear_model
+from sklearn.tree import DecisionTreeRegressor
+
+def decisiontree_regression(training_set, training_target, validation_set, validation_target):
+	prediction_model = DecisionTreeRegressor()
+	prediction_model.fit(train_data, train_target)
+	prediction = prediction_model.predict(val_data)
+    return pd.Dataframe({'p':prediction, 'a':validation_target})
 
 def linear_regression(training_set, training_target, validation_set, validation_target):
     reg = linear_model.LinearRegression()
