89e153332ccc8d4c1d92f9e62bcbe733dc708713
ariesoeteman <awsoeteman@hotmail.com>
2018-01-11 16:22:21 +0100

    calculates error

--- learning_algorithms.py
+++ learning_algorithms.py
@@ -5,20 +5,21 @@ from sklearn import neural_network
 from sklearn import linear_model
 from sklearn.tree import DecisionTreeRegressor
 
+# Train_data bestaat nog niet!!!!
 def decisiontree_regression(training_set, training_target, validation_set, validation_target):
 	prediction_model = DecisionTreeRegressor()
 	prediction_model.fit(train_data, train_target)
 	prediction = prediction_model.predict(val_data)
-    return pd.Dataframe({'p':prediction, 'a':validation_target})
+	return pd.Dataframe({'p':prediction, 'a':validation_target})
 
 def linear_regression(training_set, training_target, validation_set, validation_target):
-    reg = linear_model.LinearRegression()
-    reg.fit(training_set, training_target)
-    prediction = reg.predict(validation_set)
-    return pd.Dataframe({'p':prediction, 'a':validation_target})
+	reg = linear_model.LinearRegression()
+	reg.fit(training_set, training_target)
+	prediction = reg.predict(validation_set)
+	return pd.DataFrame({'p':prediction, 'a':validation_target})
 
 def ann_regression(training_set, training_target, validation_set, validation_target):
 	prediction_model = neural_network.MLPRegressor()
-	prediction_model.fit(train_data, train_target)
-	prediction = prediction_model.predict(val_data)
-    return pd.Dataframe({'p':prediction, 'a':validation_target})
+	prediction_model.fit(training_set, training_target)
+	prediction = prediction_model.predict(validation_set)
+	return pd.DataFrame({'p':prediction, 'a':validation_target})
